a5929a83c3a767000fb237b8ce688b31210fdde5
vega2k8s <vega2k8s@gmail.com>
2025-05-19 14:06:13 +0900

    Named import 두번째방법

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-import * as mortgage from './mortgage';
+//1) * 를 사용하여 모든 함수를 import
+//import * as mortgage from './mortgage'; 
+
+//2) calculateAmortization 함수만 import
+import { calculateAmortization } from "./mortgage";
 
 document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
@@ -7,8 +11,11 @@ document.getElementById('calcBtn').addEventListener('click', () => {
 
     // let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
     //let { monthlyPayment, monthlyRate } = calculateMonthlyPayment(principal, years, rate);
-    let { monthlyPayment, monthlyRate, amortization } = 
-        mortgage.calculateAmortization(principal, years, rate);
+    // 1)
+    //let { monthlyPayment, monthlyRate, amortization } = mortgage.calculateAmortization(principal, years, rate);
+
+    // 2)
+    let { monthlyPayment, monthlyRate, amortization } = calculateAmortization(principal, years, rate);
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
