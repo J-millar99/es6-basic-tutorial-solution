ff1d0419135e0b749512291fe44af8f25b4e968d
vega2k8s <vega2k8s@gmail.com>
2025-05-20 10:09:48 +0900

    webpack.config.js cleanplugin add

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   //entry: './js/main.js',
@@ -23,7 +24,10 @@ module.exports = {
         }
       }
     ]
-  },
+  }, //module
+  plugins: [
+    new CleanWebpackPlugin()
+  ], //plugins
   stats: {
     colors: true
   },
