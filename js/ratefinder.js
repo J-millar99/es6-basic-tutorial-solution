691ac8651973aa71b24b4503d47eda55143a9e5a
vega2k8s <vega2k8s@gmail.com>
2025-05-20 11:26:41 +0900

    axios res.data 추가

--- js/ratefinder.js
+++ js/ratefinder.js
@@ -1,6 +1,7 @@
 import * as service from './rate-service-mock';
 
 service.findAll()
+  .then(res => res.data)
   .then(rates => {
     let html = '';
     rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
