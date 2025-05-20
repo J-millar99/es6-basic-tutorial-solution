e1164ab3d95bc206e4c52876756441a0c794dae4
vega2k8s <vega2k8s@gmail.com>
2025-05-20 11:18:28 +0900

    ratefinder.js 수정

--- js/ratefinder.js
+++ js/ratefinder.js
@@ -1,10 +1,9 @@
-let url = "rates.json";
-    
-fetch(url)  //Promise
-    .then(response => response.json())
-    .then(rates => {
-      let html = '';
-      rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
-      document.getElementById("rates").innerHTML = html;
-    })
-    .catch(e => console.log(e));
+import * as service from './rate-service-mock';
+
+service.findAll()
+  .then(rates => {
+    let html = '';
+    rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
+    document.getElementById("rates").innerHTML = html;
+  })
+  .catch(e => console.log(e));
